99db26e8c84ed60e273f93ff44b60e2ebceca6fe
Ajita Shrivastava <shrivastava.ajita86@gmail.com>
2023-04-05 16:31:42 -0700

    Python code

---
+++ client1.py
@@ -0,0 +1,101 @@
+import socket
+import time
+import random
+
+# define the IP address and port number of the server
+SERVER_IP = '10.0.0.232'
+SERVER_PORT = 8000
+
+# create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the server
+client_socket.connect((SERVER_IP, SERVER_PORT))
+
+# send an initial string to the server
+client_socket.send(b'network')
+response = client_socket.recv(4096)
+print(response)
+
+window_size = 8
+packet_size = 1
+total_packets = 1000000
+max_seq_number = 65536
+curr_packet_number = 0
+dropped_packets = []
+total_sent = 0
+swt = 0
+
+def toBeOrNotToBe():
+    return (random.randint(1, 100) == 1)
+
+dropped_sequences_csv = open("dropped_sequences.csv", "w")
+dropped_sequences_csv.write("Sequence_number,timestamp\n")
+
+window_size_csv = open("window_size.csv", "w")
+window_size_csv.write("window_size,timestamp\n")
+
+retransmission_sequences_csv = open("retransmission_sequences.csv", "w")
+retransmission_sequences_csv.write("retransmission_sequences,timestamp\n")
+
+if response == b'success':
+    print("Let the games begin!!!")
+
+    while(total_sent < total_packets):
+        recv_all_packets = True
+        if curr_packet_number*packet_size >= max_seq_number:
+            curr_packet_number = 0
+
+        if swt >= 100:
+            # send stage
+            dropped_packets_new = []
+            index = 0
+            while(index < len(dropped_packets)):
+                if(not toBeOrNotToBe()):
+                    packet = dropped_packets[index]
+                    client_socket.send(str.encode(packet))
+                    del dropped_packets[index]
+                    print("Retransmission: ",curr_packet_number, "- " , packet)
+                    retransmission_sequences_csv.write("{},{}\n".format(packet, time.time()))
+                    # print("RESEND --->", packet)
+                    ack = client_socket.recv(4096)
+                else:
+                    index += 1
+                    dropped_packets_new.append(str(seq_number))
+                    print("DROPPED", str(seq_number))
+                    dropped_sequences_csv.write("{},{}\n".format(seq_number, time.time()))
+            
+            dropped_packets.extend(dropped_packets_new)
+
+        to_send = []
+
+        window_size_csv.write("{},{}\n".format(window_size, time.time()))
+
+        for i in range(window_size):
+            seq_number = curr_packet_number*packet_size+1
+
+            if not toBeOrNotToBe():
+                # recv_all_packets = False
+                to_send.append(str(seq_number))
+            else:
+                if window_size > 1:
+                    window_size = int(window_size / 2)
+                print ("window_size: ", window_size)
+                dropped_packets.append(str(seq_number))
+                print("DROPPED", str(seq_number))
+                dropped_sequences_csv.write("{},{}\n".format(seq_number, time.time()))
+
+            curr_packet_number = curr_packet_number + 1
+        total_sent += len(to_send)
+        swt += len(to_send)
+        random.shuffle(to_send)
+
+        # send stage
+        for packet in to_send:
+            client_socket.send(str.encode(packet))
+            ack = client_socket.recv(4096)
+        
+        if (recv_all_packets):
+            window_size = window_size * 2
+
+
---
+++ server1.py
@@ -0,0 +1,99 @@
+import socket
+import threading
+import bisect
+import time
+
+# define the IP address and port number
+IP_ADDRESS = '0.0.0.0'
+PORT = 8000
+
+# create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Check if needed or not
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# bind the socket object to the IP address and port number
+server_socket.bind((IP_ADDRESS, PORT))
+
+# listen for incoming connections
+server_socket.listen(1)
+
+# accept incoming connections
+print("Waiting for client to connect on port: ", PORT)
+
+# display client's IP address and port number
+# print(f"Connected to client: {client_address[0]}:{client_address[1]}")
+
+packets_received = []
+packet_counter = 0
+last_prcoessed = 0
+max_sequence_number = 65536
+packet_size = 1
+
+def cal_metrics():
+    global last_prcoessed
+    global packets_received
+    last_processed_seq_no = packets_received[last_prcoessed]
+    dropped_packets = 0
+    for i in range(last_prcoessed + 1, last_prcoessed + 1000):
+        if packets_received[i] == last_processed_seq_no + packet_size:
+            last_processed_seq_no = packets_received[i]
+        else:
+            dropped_packets = dropped_packets + 1
+            last_processed_seq_no = last_processed_seq_no + packet_size
+    print("Packet {} - {}: Dropped Packet - {}, Good Put - {}".format(last_prcoessed, last_prcoessed+1000, (dropped_packets)/1000, (1-((dropped_packets)/1000))))
+
+seq_f = open("sequence_received.csv", "w")
+seq_f.write("seqno,tp\n")
+
+def handle_client_connection(client_socket): 
+    global packet_counter
+    global last_prcoessed
+    global packets_received
+    request = client_socket.recv(1024)
+    print ('Received {}'.format(request))
+    if request == b'network':
+        client_socket.send(b'success')
+        while True:
+            seq_no = client_socket.recv(1024)
+            if not seq_no:  # no data received within timeout period
+                print('Client stopped sending data. Closing socket.')
+                client_socket.close()
+                break
+            seq_no = seq_no.decode()
+            bisect.insort(packets_received, int(seq_no))
+            # print(packets_received)
+            if packet_counter == 1000:
+                packet_counter = 0
+                cal_metrics()
+                if len(packets_received) >= max_sequence_number / packet_size:
+                    packets_received = []
+                    last_prcoessed = 0
+                else:
+                    last_prcoessed = last_prcoessed + 1000
+
+            seq_f.write("{},{}\n".format(seq_no, time.time()))
+
+            client_socket.send(str.encode(str(int(seq_no)+packet_size)))
+            # print(packets_received)
+            packet_counter = packet_counter + 1
+
+while True:
+    client_sock, address = server_socket.accept()
+    print ('Accepted connection from {}:{}'.format(address[0], address[1]))
+    try: 
+        client_handler = threading.Thread(
+
+            target=handle_client_connection,
+            args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
+        )
+        client_handler.start()
+        client_handler.join() 
+    except: 
+        print("Client left.")
+        client_sock.close()
+        # seq_f.close()
+    finally:
+        print("All of the client's data is received")
+        client_sock.close()
+        break
